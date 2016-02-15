c1653d003563b03d7c9996f1bb979bc13e6dc8fe
FlorianGoussin <florian.goussin@gmail.com>
2016-02-14 18:40:01 -0700

    default scss and fix some problems in main php file

--- hermes-contact-form.php
+++ hermes-contact-form.php
@@ -26,39 +26,37 @@ define('SEND_EMAIL_ERROR_MESSAGE_OPTION', 'send_email_error_message');
 include "send-email.php";
 include "update-settings.php";
 
-// both: front end and wp-admin
+
+// src css front and back ends
+add_action('init', 'hermes_contact_form_styles_both');
+function hermes_contact_form_styles_both() {
+	wp_register_style('hermes_contact_form_css', WEBSITE_SNAPSHOT__PLUGIN_URL.'css/style.css');
+	wp_enqueue_style('hermes_contact_form_css');
+}
+
+// src js front and back ends
 add_action('init', 'hermes_contact_form_enqueue_scripts_both');
-
-// src js and css files for the front end side
-add_action('wp_enqueue_scripts', 'hermes_contact_form_enqueue_scripts');
-add_action('wp_enqueue_styles', 'hermes_contact_form_enqueue_styles');
- 
-// src js and css files for the wp-admin side (settings)
-add_action('admin_init', 'hermes_contact_form_settings_enqueue_scripts');
-add_action('admin_init', 'hermes_contact_form_settings_enqueue_styles');
-
 function hermes_contact_form_enqueue_scripts_both() {
-	wp_register_script('validation_js', WEBSITE_SNAPSHOT__PLUGIN_URL . 'node_modules/validate-js/validate.min.js', null, null, false);
+	wp_register_script('validation_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'node_modules/validate-js/validate.min.js', null, null, false);
   wp_enqueue_script('validation_js');
 }
 
+// src js and css files for the front end side
+add_action('wp_enqueue_scripts', 'hermes_contact_form_enqueue_scripts');
 function hermes_contact_form_enqueue_scripts() {
-	wp_register_script('hermes_contact_form_js', WEBSITE_SNAPSHOT__PLUGIN_URL . 'js/contact-form.js', null, null, true);
+	wp_register_script('hermes_contact_form_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'js/contact-form.js', null, null, true);
   wp_enqueue_script('hermes_contact_form_js');
 }
 
-function hermes_contact_form_enqueue_styles() {}
-
+// src js and css files for the wp-admin side (settings)
+add_action('admin_init', 'hermes_contact_form_settings_enqueue_scripts');
 function hermes_contact_form_settings_enqueue_scripts() {
-  wp_register_script('hermes_contact_form_admin_ui_js', WEBSITE_SNAPSHOT__PLUGIN_URL . 'js/admin-ui.js', null, null, true);
+  wp_register_script('hermes_contact_form_admin_ui_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'js/admin-ui.js', null, null, true);
   wp_enqueue_script('hermes_contact_form_admin_ui_js');
 }
 
-function hermes_contact_form_settings_enqueue_styles() {}
-
 // Settings page
 add_action('admin_menu', function() {
-	// add_menu_page('Contact Form Options', 'Contact Form Options', 'manage_options', __FILE__, 'hermes_contact_form_settings_view');
 	add_options_page(PLUGIN_NAME, PLUGIN_NAME, 'manage_options', __FILE__, 'hermes_contact_form_settings_view');
 });
 
