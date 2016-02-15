2e2b03f2d033f7c38a56ef843c195b459913d5ca
FlorianGoussin <florian.goussin@gmail.com>
2016-02-14 18:38:49 -0700

    move output at the bottom

--- views/_contact-form.php
+++ views/_contact-form.php
@@ -1,21 +1,21 @@
 <div class="hermes-contact-form-wrap">
 	<form id="hermes-contact-form" novalidate>
-		<div class="output"></div>
-		<div class="name">
+		<div class="name input-group">
 			<label for="visitor-name">Name</label>
 			<input id="visitor-name" type="text">
 		</div>
-		<div class="email">
+		<div class="email input-group">
 			<label for="visitor-email">Email</label>
 			<input id="visitor-email" type="text">
 		</div>
-		<div class="message">
+		<div class="message input-group">
 			<label for="visitor-message">Message</label>
 			<textarea id="visitor-message" cols="30" rows="10"></textarea>
 		</div>
-		<div class="submit">
+		<div class="submit input-group">
 			<button type="submit">Submit</button>
 			<?php wp_nonce_field('contact_form_token', 'csrf_token'); ?>
 		</div>
+		<div class="output input-group"></div>
 	</form>
 </div>
