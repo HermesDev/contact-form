81daf429d8929e69d769ecf62c9093ad50afab7e
Florian Goussin <florian@hermesdevelopment.com>
2016-02-16 21:26:22 +0000

    contact form is back to work with namespaces

--- main.php
+++ main.php
@@ -9,56 +9,13 @@ Author URI: http://hermesdevelopment.com
 License: GPLv2
 */
 
+namespace hermes_dev\contact_form;
+
+include 'php/classes/plugin.class.php';
+include 'php/classes/contact-form.class.php';
+// include 'php/classes/settings.class.php';
+
 include 'php/_constants.php';
 include 'php/utils.php';
-include 'php/send-email.php';
-// include 'php/update-settings.php';
-
-
-// src css front and back ends
-add_action('init', 'hermes_contact_form_styles_both');
-function hermes_contact_form_styles_both() {
-	wp_register_style('hermes_contact_form_css', WEBSITE_SNAPSHOT__PLUGIN_URL.'css/style.css');
-	wp_enqueue_style('hermes_contact_form_css');
-}
-
-// src js front and back ends
-add_action('init', 'hermes_contact_form_enqueue_scripts_both');
-function hermes_contact_form_enqueue_scripts_both() {
-	wp_register_script('validation_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'node_modules/validate-js/validate.min.js', null, null, false);
-  wp_enqueue_script('validation_js');
-}
-
-// src js and css files for the front end side
-add_action('wp_enqueue_scripts', 'hermes_contact_form_enqueue_scripts');
-function hermes_contact_form_enqueue_scripts() {
-	wp_register_script('hermes_contact_form_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'js/contact-form.js', null, null, true);
-  wp_enqueue_script('hermes_contact_form_js');
-}
-
-// src js and css files for the wp-admin side (settings)
-add_action('admin_init', 'hermes_contact_form_settings_enqueue_scripts');
-function hermes_contact_form_settings_enqueue_scripts() {
-  wp_register_script('hermes_contact_form_admin_ui_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'js/admin-ui.js', null, null, true);
-  wp_enqueue_script('hermes_contact_form_admin_ui_js');
-}
-
-// Settings page
-// add_action('admin_menu', function() {
-// 	add_options_page(PLUGIN_NAME, PLUGIN_NAME, 'manage_options', __FILE__, 'hermes_contact_form_settings_view');
-// });
-
-// function hermes_contact_form_settings_view() {
-// 	include 'views/_admin-ui.php';
-// }
-
-// Contact form
-add_action('init', function() {
-	add_shortcode('hermes_contact_form', 'hermes_contact_form_main_view');
-});
-
-function hermes_contact_form_main_view() {
-	include 'views/_contact-form.php';
-}
 
 ?>
---
+++ php/classes/plugin.class.php
@@ -0,0 +1,64 @@
+<?php 
+
+Plugin::init();
+
+/**
+ * Plugin WordPress Plugin Entry point
+ */
+class Plugin {
+  public static function init() {
+  	add_action ('init', array(__CLASS__, 'init_tasks'));
+		add_action('admin_init', array(__CLASS__, 'admin_init_tasks'));
+		add_action('wp_footer', array(__CLASS__, 'wp_footer_tasks'));
+		// add_action('admin_menu', array(__CLASS__, 'wp_admin_menu_tasks'));
+  }
+
+  /**
+   * init_tasks Tasks related to WordPress init action
+   */
+  public static function init_tasks() {
+  	wp_enqueue_script('validation_js', 
+  		WEBSITE_SNAPSHOT__PLUGIN_URL.'node_modules/validate-js/validate.min.js', null, null, false);
+  	wp_enqueue_style('hermesdev_contact_form_css', WEBSITE_SNAPSHOT__PLUGIN_URL.'css/style.css');
+
+  	add_shortcode('hermes_contact_form', array(__CLASS__, 'show_contact_form_view')); // Contact form shortcode
+  }
+
+  /**
+   * admin_init_tasks Tasks related to WordPress admin_init action
+   */
+  public static function admin_init_tasks() {
+  	wp_enqueue_script('hermesdev_contact_form_settings_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'js/settings.js', 
+  		null, null, true);
+  }
+
+  /**
+   * wp_footer_tasks Tasks related to WordPress wp_footer action
+   */
+  public static function wp_footer_tasks() {
+  	wp_enqueue_script('hermesdev_contact_form_js', WEBSITE_SNAPSHOT__PLUGIN_URL.'js/contact-form.js', 
+  		null, null, true);
+  }
+
+  /**
+   * wp_admin_menu_tasks Tasks related to WordPress wp_admin_menu action
+   */
+  public static function wp_admin_menu_tasks() {
+  	add_options_page(PLUGIN_NAME, PLUGIN_NAME, 'manage_options', __FILE__, 
+  		array(__CLASS__, 'show_settings_view'));
+  }
+
+  /**
+   * show_contact_form_view Show the main view with the contact form
+   */
+	public static function show_contact_form_view() {
+		include dirname(__FILE__).'/../../views/_contact-form-template.php';
+	}
+
+	/**
+	 * show_settings_view Show the admin view with the contact form settings
+	 */
+	public static function show_settings_view() {
+		include dirname(__FILE__).'/../../views/_settings-template.php';
+	}
+}
